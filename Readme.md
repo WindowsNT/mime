205ee300e1a6b7c995f280c9339484634d43fde2
Michael Chourdakis <m@turboirc.comc>
2018-09-03 18:34:01 +0300

    Add project files.

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# Mime
+A C++ library for Windows for MIME. Easily portable to other platforms. Uses AdES (https://github.com/WindowsNT/AdES) for signing if needed.
+
+## Getting Started
+
+Description, examples etc at CodeProject: https://www.codeproject.com/Articles/1114232/Cplusplus-MIME-A-simple-single-header-parser-an
+git clone url
+git submodule init
+git submodule update
+
---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <memory>
+#include <sstream>
+#include <functional>
+using namespace std;
+
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#include <windows.h>
+// #define MIME_ADES
+#include "mime2.h"
+
+int main()
+{
+
+}
